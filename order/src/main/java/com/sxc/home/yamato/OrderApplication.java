53d13f0578af80b250eed90d669b91cfbf1b70a1
shenxiaochao <shenxiaochao@ehousechina.com>
2019-03-02 15:55:07 +0800

    initial commit

---
+++ order/src/main/java/com/sxc/home/yamato/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.sxc.home.yamato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
